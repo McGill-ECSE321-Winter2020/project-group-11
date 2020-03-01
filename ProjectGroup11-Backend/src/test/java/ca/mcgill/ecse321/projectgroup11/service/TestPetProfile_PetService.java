7ba5337048f7d0eb62ecb5146d65806de2de13d7
keanu-natchev <keanu.natchev@mail.mcgill.ca>
2020-03-01 12:05:31 -0500

    Adding further tests for TestPetProfile_PetService

--- ProjectGroup11-Backend/src/test/java/ca/mcgill/ecse321/projectgroup11/service/TestPetProfile_PetService.java
+++ ProjectGroup11-Backend/src/test/java/ca/mcgill/ecse321/projectgroup11/service/TestPetProfile_PetService.java
@@ -43,6 +43,32 @@ public class TestPetProfile_PetService {
 	@InjectMocks
 	private PetService service;
 
+	
+	public static final Integer USER_ID = 5;
+	public static final String USER_NAME = "Bobo";
+	public static final String USER_TYPE = "cat";
+	public static final String USER_DESCRIPTION = "chubby";
+	public static final String USER_PHOTOURL = "urlurl";
+	public static final String USER_BREED = "street cat";
+	public static final Boolean USER_APARTMENT = true;
+	public static final Boolean USER_KIDOK = true;
+	public static final Boolean USER_PETOK = true;
+	public static final Boolean USER_HIGHE = false;
+	public static final String USER_HEALTH = "chubby but healthy boi";
+	
+	public static final Integer NONEXISTING_ID = 20;
+	public static final String NONEXISTING_NAME = "Caesar";
+	public static final String NONEXISTING_TYPE = "dog";
+	public static final String NONEXISTING_DESCRIPTION = "lazy";
+	public static final String NONEXISTING_PHOTOURL = "urlurlurl";
+	public static final String NONEXISTING_BREED = "German Shepherd";
+	public static final Boolean NONEXISTING_APARTMENT = true;
+	public static final Boolean NONEXISTING_KIDOK = true;
+	public static final Boolean NONEXISTING_PETOK = true;
+	public static final Boolean NONEXISTING_HIGHE = false;
+	public static final String NONEXISTING_HEALTH = "Young healthy dog";
+	
+	
 	private static final int USER_KEY = 5;
 	private static final int NONEXISTING_KEY = 20;
 
@@ -50,10 +76,21 @@ public class TestPetProfile_PetService {
 	public void setUp() throws Exception {
 		//When finding by ID - return an Adopter with ID USER_KEY if passed with ID user_key
 		lenient().when(petProfileDao.findPetProfileById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
-			if (invocation.getArgument(0).equals(USER_KEY)) {
-				Shelter shelter = new Shelter();
-				shelter.setId(USER_KEY);
-				return shelter;
+			if (invocation.getArgument(0).equals(USER_ID)) {
+				PetProfile petProfile = new PetProfile();
+				petProfile.setId(USER_ID);
+				petProfile.setName(USER_NAME);
+				petProfile.setType(USER_TYPE);
+				petProfile.setDescription(USER_DESCRIPTION);
+				petProfile.setPhotoURL(USER_PHOTOURL);
+				petProfile.setBreed(USER_BREED);
+				petProfile.setApartment(USER_APARTMENT);
+				petProfile.setKidsOkay(USER_KIDOK);
+				petProfile.setPetsOkay(USER_PETOK);
+				petProfile.setHighEnergy(USER_HIGHE);
+				petProfile.setHealthConcerns(USER_HEALTH);
+				
+				return petProfile;
 			} else {
 				return null;
 			}
@@ -180,6 +217,22 @@ public class TestPetProfile_PetService {
 		assertNull(petProfile);
 		//To correct
 		assertEquals("PetProfile cannot be empty!", error);
+		
+	}
+	
+	@Test
+	public void testCreatePetProfileWithSameInfo() {
+		PetProfile p = null;
+		try {
+			p = service.createPetProfile(USER_ID, USER_NAME, USER_TYPE, USER_DESCRIPTION, USER_PHOTOURL, USER_BREED, USER_APARTMENT, USER_KIDOK, USER_PETOK, USER_HIGHE, USER_HEALTH);
+		} catch (Exception e) {
+			assertNull(p);
+			assertEquals(e.getMessage(), "Profile with same ID already exists");
+		}
+	}
+	
+	@Test
+	public void testGetPetProfiles() {
 		
 	}
 	
